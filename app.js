ecd2dc5c12895b54ef2cba6f4f752fc0892de550
Jonathan <jalanbunker@gmail.com>
2024-06-07 11:46:10 -0600

    Added basic click functionality

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ cells.forEach((cell) => {
   cell.onclick = () => {
     if (current_turn % 2 === 0) {
       cell.classList.add("x");
+      cell.innerHTML = "x";
     } else {
       cell.classList.add("o");
+      cell.innerHTML = "o";
     }
 
     current_turn += 1;
