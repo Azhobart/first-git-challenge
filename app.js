5023e08b150ad45ad37e7c59325f29db39d90d5c
Jonathan <jalanbunker@gmail.com>
2024-06-07 12:41:41 -0600

    Added falling tiles

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 let cells = document.querySelectorAll(".tile");
+let board = document.getElementById("board");
 
 let current_turn = 0;
+const board_width = 7;
+const board_height = 6;
 
 cells.forEach((cell) => {
   cell.onclick = () => {
     if (current_turn % 2 === 0) {
-      cell.classList.add("x");
-      cell.innerHTML = "x";
-    } else {
       cell.classList.add("o");
-      cell.innerHTML = "o";
+    } else {
+      cell.classList.add("x");
     }
 
     current_turn += 1;
+
+    for (let x = 0; x < board_width; x++) {
+      for (let y = 0; y < board_height; y++) {
+        let current_cell = board.children[y].children[x];
+
+        if (
+          current_cell.classList.contains("o") ||
+          current_cell.classList.contains("x")
+        ) {
+          if (y < board_height - 1) {
+            let lower_cell = board.children[y + 1].children[x];
+            if (
+              !lower_cell.classList.contains("o") &&
+              !lower_cell.classList.contains("x")
+            ) {
+              if (current_cell.classList.contains("o")) {
+                lower_cell.classList.add("o");
+                current_cell.classList.remove("o");
+              }
+              if (current_cell.classList.contains("x")) {
+                lower_cell.classList.add("x");
+                current_cell.classList.remove("x");
+              }
+            }
+          }
+        }
+      }
+    }
   };
 });
